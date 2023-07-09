d6dd415f767f13116e7ce47005af02fadb7fccf7
Tenzin Norden <nordentenzin29@gmail.com>
2023-07-09 12:33:22 +0530

    Initial Commit

---
+++ AnimalCrossingTinder/AnimalCrossingTinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalCrossingTinderApp.swift
+//  AnimalCrossingTinder
+//
+//  Created by Tenzin Norden on 7/9/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimalCrossingTinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
